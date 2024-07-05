9b0f65dd0b658edc8649c66bed9057acd5bcf4cd
Elwin Roman <elwinromanv@gmail.com>
2024-07-05 01:53:39 -0500

    feat: Update social links in icons and color brand hover effect

--- src/constants/social-links.js
+++ src/constants/social-links.js
@@ -13,7 +13,9 @@ export const links = [
             stroke-linecap="round"
             stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
-          </svg>`
+          </svg>`,
+    brandColor1TailwindClass: 'hover:bg-[#24292e]',
+    brandColor2TailwindClass: 'group-hover:text-[#fafbfc]'
   },
   { 
     name: 'Linkedin',
@@ -28,7 +30,9 @@ export const links = [
             stroke-linecap="round"
             stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M8 11l0 5" /><path d="M8 8l0 .01" /><path d="M12 16l0 -5" /><path d="M16 16v-3a2 2 0 0 0 -4 0" />
-          </svg>`	 
+          </svg>`,
+    brandColor1TailwindClass: 'hover:bg-[#0077B5]',
+    brandColor2TailwindClass: 'group-hover:text-[#FFFFFF]'
   },
   {
     name: 'elwinromanv@gmail.com',
@@ -43,7 +47,9 @@ export const links = [
             stroke-linecap="round"
             stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" /><path d="M3 7l9 6l9 -6" />
-          </svg>`
+          </svg>`,
+    brandColor1TailwindClass: 'hover:bg-[#d44638]',
+    brandColor2TailwindClass: 'group-hover:text-[#e5e4e2]'
   },
   {
     name: '𝕏 Twitter',
@@ -58,7 +64,9 @@ export const links = [
             stroke-linecap="round"
             stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4l11.733 16h4.267l-11.733 -16z" /><path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
-          </svg>`
+          </svg>`,
+    brandColor1TailwindClass: 'hover:bg-[#000000]',
+    brandColor2TailwindClass: 'group-hover:text-[#ffffff]'
   },
   {
     name: 'Spotify',
@@ -73,6 +81,8 @@ export const links = [
             stroke-linecap="round"  
             stroke-linejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M8 11.973c2.5 -1.473 5.5 -.973 7.5 .527" /><path d="M9 15c1.5 -1 4 -1 5 .5" /><path d="M7 9c2 -1 6 -2 10 .5" />
-          </svg>`
+          </svg>`,
+    brandColor1TailwindClass: 'hover:bg-[#1DB954]',
+    brandColor2TailwindClass: 'group-hover:text-[#FFFFFF]'
   }
 ]
