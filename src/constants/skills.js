e4bf16f10017362d3d8cf98a6118649abc097d33
Elwin Roman <elwinromanv@gmail.com>
2024-07-06 22:28:30 -0500

    refactor: Update Skills component to use dynamic data

---
+++ src/constants/skills.js
@@ -0,0 +1,62 @@
+export const skills = [
+  {
+    name: 'JavaScript',
+    path: 'svg-icons/javascript.svg',
+    alt: 'JavaScript logo',
+    active: true
+  },
+  {
+    name: 'CSS',
+    path: 'svg-icons/css.svg',
+    alt: 'CSS logo',
+    active: true
+  },
+  {
+    name: 'React JS',
+    path: 'svg-icons/reactjs.svg',
+    alt: 'React JS logo',
+    active: true
+  },
+  {
+    name: 'PHP',
+    path: 'svg-icons/php.svg',
+    alt: 'PHP logo',
+    active: true
+  },
+  {
+    name: 'Git',
+    path: 'svg-icons/git.svg',
+    alt: 'Git logo',
+    active: true
+  },
+  {
+    name: 'Tailwind CSS',
+    path: 'svg-icons/tailwindcss.svg',
+    alt: 'Tailwind CSS logo',
+    active: true
+  },
+  {
+    name: 'Laravel',
+    path: 'svg-icons/laravel.svg',
+    alt: 'Laravel logo',
+    active: true
+  },
+  {
+    name: 'MySQL',
+    path: 'svg-icons/mysql.svg',
+    alt: 'MySQL logo',
+    active: true
+  },
+  {
+    name: 'Astro JS',
+    path: 'svg-icons/astrojs.svg',
+    alt: 'Astro JS logo',
+    active: true
+  },
+  {
+    name: 'TypeScript',
+    path: 'svg-icons/typescript.svg',
+    alt: 'TypeScript logo',
+    active: true
+  },
+]
