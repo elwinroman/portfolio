901390db3a2eac344619a56af241f092706b5692
Elwin Roman <elwinromanv@gmail.com>
2024-07-15 22:24:29 -0500

    refactor: Update Skills component to use dynamic data and improve layout

--- src/constants/skills.js
+++ src/constants/skills.js
@@ -1,62 +1,83 @@
-export const skills = [
+export const mainSkills = [
   {
     name: 'JavaScript',
     path: 'svg-icons/javascript.svg',
     alt: 'JavaScript logo',
-    active: true
-  },
-  {
-    name: 'CSS',
-    path: 'svg-icons/css.svg',
-    alt: 'CSS logo',
-    active: true
-  },
-  {
-    name: 'React JS',
-    path: 'svg-icons/reactjs.svg',
-    alt: 'React JS logo',
-    active: true
-  },
-  {
-    name: 'PHP',
-    path: 'svg-icons/php.svg',
-    alt: 'PHP logo',
-    active: true
-  },
-  {
-    name: 'Git',
-    path: 'svg-icons/git.svg',
-    alt: 'Git logo',
-    active: true
-  },
-  {
-    name: 'Tailwind CSS',
-    path: 'svg-icons/tailwindcss.svg',
-    alt: 'Tailwind CSS logo',
-    active: true
-  },
-  {
-    name: 'Laravel',
-    path: 'svg-icons/laravel.svg',
-    alt: 'Laravel logo',
-    active: true
-  },
-  {
-    name: 'MySQL',
-    path: 'svg-icons/mysql.svg',
-    alt: 'MySQL logo',
-    active: true
-  },
-  {
-    name: 'Astro JS',
-    path: 'svg-icons/astrojs.svg',
-    alt: 'Astro JS logo',
-    active: true
+    active: true,
   },
   {
     name: 'TypeScript',
     path: 'svg-icons/typescript.svg',
     alt: 'TypeScript logo',
-    active: true
+    active: true,
+  },
+  {
+    name: 'React JS',
+    path: 'svg-icons/reactjs.svg',
+    alt: 'React JS logo',
+    active: true,
+  },
+  {
+    name: 'Node JS',
+    path: 'svg-icons/nodejs.svg',
+    alt: 'NodeJS logo',
+    active: true,
+  },
+  {
+    name: 'CSS',
+    path: 'svg-icons/css.svg',
+    alt: 'CSS logo',
+    active: true,
+  },
+  {
+    name: 'NextJS',
+    path: 'svg-icons/nextjs.svg',
+    alt: 'NextJS logo',
+    active: true,
+  },
+  {
+    name: 'Git',
+    path: 'svg-icons/git.svg',
+    alt: 'Git logo',
+    active: true,
+  },
+  {
+    name: 'SQL Server',
+    path: 'svg-icons/sqlserver.svg',
+    alt: 'SQL Server logo',
+    active: true,
+  },
+]
+
+export const otherSkills = [
+  {
+    name: 'Tailwind CSS',
+    path: 'svg-icons/tailwindcss.svg',
+    alt: 'Tailwind CSS logo',
+    active: true,
+  },
+  {
+    name: 'Astro JS',
+    path: 'svg-icons/astrojs.svg',
+    alt: 'Astro JS logo',
+    active: true,
+  },
+  {
+    name: 'MySQL',
+    path: 'svg-icons/mysql.svg',
+    alt: 'MySQL logo',
+    active: true,
+  },
+  {
+    name: 'PHP',
+    path: 'svg-icons/php.svg',
+    alt: 'PHP logo',
+    active: true,
+  },
+  {
+    name: 'Laravel',
+    path: 'svg-icons/laravel.svg',
+    alt: 'Laravel logo',
+    active: false,
   },
 ]
